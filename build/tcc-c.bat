a52f43868f92702922b598b86c6bb88972630616
bumbread <thebumboni@gmail.com>
2020-10-20 04:26:23 +1100

    fixed tcc script

--- build/tcc-c.bat
+++ build/tcc-c.bat
@@ -3,4 +3,4 @@
 set defines=-D_CRT_SECURE_NO_WARNINGS -D_UNICODE -DUNICODE
 set lnk_libs= -lkernel32 -luser32 -lgdi32 -lshell32 -lshlwapi
 
-tcc -w -g -b %input_file_name% -o %output_file_name% %defines% %lnk_libs%
+tcc -w -g -b %input_file_name% -o %output_file_name%.exe %defines% %lnk_libs%
