3c5b17f03b568df9f019cbbd21079a57e46ef468
bumbread <thebumboni@gmail.com>
2020-10-21 03:04:31 +1100

    removed stdio header include

--- src/imgvu/config.c
+++ src/imgvu/config.c
@@ -1,8 +1,4 @@
 
-#pragma warning(push, 1)
-#include<stdio.h>
-#pragma warning(pop)
-
 internal void app_load_default_config(struct t_app_config* appConfig, t_string16 filename) {
   // load the default values into appConfig struct and then save as filename
   debug_variable_unused(appConfig);
@@ -11,7 +7,7 @@ internal void app_load_default_config(struct t_app_config* appConfig, t_string16
 
 internal void app_load_config(struct t_app_config* appConfig, t_string16 filename) {
   debug_variable_unused(appConfig);
-  wprintf(filename.ptr);
+  
   t_file_data configData = platform_load_file(filename);
   if(configData.size) {
     
