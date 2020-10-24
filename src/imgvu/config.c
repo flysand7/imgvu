3e4691e3ea4eca370838859651700064b7cc9054
bumbread <thebumboni@gmail.com>
2020-10-24 23:42:53 +1100

    Finish lexing

--- src/imgvu/config.c
+++ src/imgvu/config.c
@@ -37,6 +37,7 @@ struct t_token {
 };
 
 struct t_token_list {
+  bool error;
   u32 tokensCount;
   u32 tokensAlloc;
   struct t_token* tokens;
@@ -56,9 +57,11 @@ internal void in_range(char x, char y, char c) {
   return(c >= x && c <= y);
 }
 
-internal void app_parse_config(struct t_app_config* appConfig, t_file_data data) {
-  appConfig->error = false;
-  
+internal void is_operator(char c) {
+  return(c=='+' || c=='-' || c=='*' || c=='/' || c=='=');
+}
+
+internal struct t_token_list app_lex_config(struct t_app_config* appConfig, t_file_data data) {
   char* characters = (char*)data->ptr;
   u32 pos = 0;
   struct t_token currentToken = {0};
@@ -100,6 +103,41 @@ internal void app_parse_config(struct t_app_config* appConfig, t_file_data data)
       currentToken.type = TYPE_STRING;
       goto state_string;
     }
+    else if(currentChar == '(') {
+      pos += 1;
+      currentToken.len += 1;
+      currentToken.type = TYPE_LPAREN;
+      token_push(&list, currentToken);
+      goto state_main;
+    }
+    else if(currentChar == ')') {
+      pos += 1;
+      currentToken.len += 1;
+      currentToken.type = TYPE_RPAREN;
+      token_push(&list, currentToken);
+      goto state_main;
+    }
+    else if(currentChar == '{') {
+      pos += 1;
+      currentToken.len += 1;
+      currentToken.type = TYPE_LARR;
+      token_push(&list, currentToken);
+      goto state_main;
+    }
+    else if(currentChar == '(') {
+      pos += 1;
+      currentToken.len += 1;
+      currentToken.type = TYPE_PARR;
+      token_push(&list, currentToken);
+      goto state_main;
+    }
+    else if(is_operator(currentChar)) {
+      pos += 1;
+      currentToken.len += 1;
+      currentToken.type = TYPE_OPERATOR;
+      token_push(&list, currentToken);
+      goto state_main;
+    }
     else {
       pos += 1;
       goto state_main;
@@ -135,7 +173,7 @@ internal void app_parse_config(struct t_app_config* appConfig, t_file_data data)
     }
   }
   
-  // TODO(bumbread): do i want 0x to mean 0???
+  // TODO(bumbread): do i want 0x to mean 0 ???
   state_integer_hex: {
     char currentChar = characters[pos];
     if(0
@@ -254,15 +292,21 @@ internal void app_parse_config(struct t_app_config* appConfig, t_file_data data)
     }
   }
   
+  // TODO(bumbread): error messages
   lex_error: {
     currentToken.len += 1;
     currentToken.type = TYPE_ERROR;
-    return;
+    list.error = true;
+    return list;
   }
   
   exit: {
-    return;
+    return list;
   }
+}
+
+internal bool app_parse_config(struct t_token_list tokenList, t_app_config* appConfig) {
+  
 }
 
 internal void app_load_config(struct t_app_config* appConfig, t_string16 filename) {
@@ -272,8 +316,8 @@ internal void app_load_config(struct t_app_config* appConfig, t_string16 filenam
   t_file_data configData = platform_load_file(filename);
   if(configData.size) {
     struct t_app_config tempConfig = {0};
-    app_parse_config(&tempConfig, configData);
-    if(appConfig->error) {
+    t_token_list configTokens = app_lex_config(&tempConfig);
+    if(configTokens.error) {
       // TODO(bumbread): error handle this
     }
     else {
